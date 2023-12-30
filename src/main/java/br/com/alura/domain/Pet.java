12861b7ab94c9c6fca2833441faa056a8072777b
Luan Valente <luanbvalente@hotmail.com>
2023-12-30 13:19:01 -0300

    Jackson-databind

--- src/main/java/br/com/alura/domain/Pet.java
+++ src/main/java/br/com/alura/domain/Pet.java
@@ -2,12 +2,16 @@ package br.com.alura.domain;
 
 public class Pet {
 
+    private Long id;
     private String tipo;
     private String nome;
     private String raca;
     private int idade;
     private String cor;
     private Float peso;
+
+    public Pet() {
+    }
 
     public Pet(String tipo, String nome, String raca, int idade, String cor, Float peso) {
         this.tipo = tipo;
@@ -17,4 +21,33 @@ public class Pet {
         this.cor = cor;
         this.peso = peso;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public Float getPeso() {
+        return peso;
+    }
+    
 }
